9c78201be8fd754f7c406eea0b198f7f43d87c22
Josiah Luna <josiah.luna@live.com>
2022-03-30 22:44:59 -0700

    add additional functions for game states etc.

--- twirp_example.py
+++ twirp_example.py
@@ -6,8 +6,44 @@ from proto import baddle_twirp, baddle_pb2
 
 client = baddle_twirp.BaddleClient("http://localhost:2441")
 
-try:
-    response = client.GetWord(ctx=Context(), request=baddle_pb2.GetWordReq(user="test-user"))
-    print(response)
-except TwirpServerException as e:
-    print(e.code, e.message, e.meta, e.to_dict())
+def get_word(user_id, rm_num):
+    try:
+        response = client.GetWord(
+            ctx=Context(), request=baddle_pb2.GetWordReq(user=user_id)
+        )
+        print(response)
+        return response
+    except TwirpServerException as e:
+        print(e.code, e.message, e.meta, e.to_dict())
+
+
+def send_loss(rm_num):
+    try:
+        response = client.Lose(
+            ctx=Context(), request=baddle_pb2.LoseReq(room=rm_num)
+        )
+        print(response)
+        return response
+    except TwirpServerException as e:
+        print(e.code, e.message, e.meta, e.to_dict())
+
+
+def game_state_req(user_id, rm_num):
+    try:
+        response = client.GetGameState(
+            ctx=Context(), request=baddle_pb2.GetGameStateReq(user=user_id, room=rm_num)
+        )
+        print(response)
+        return response
+    except TwirpServerException as e:
+        print(e.code, e.message, e.meta, e.to_dict())
+
+def join_room(user_id, rm_num):
+    try:
+        response = client.JoinRoom(
+            ctx=Context(), request=baddle_pb2.JoinRoomReq(user=user_id, room=rm_num)
+        )
+        print(response)
+        return response
+    except TwirpServerException as e:
+        print(e.code, e.message, e.meta, e.to_dict())
